ce3c6e1c3663d7fdbe67d27b741f8447ca1789da
davbt <davidbatonda7@gmail.com>
2026-06-22 11:21:17 +1000

    Improve compatibility of docker container
    Previously, the Dockerfile would install yq amd64 version and ansible
    automatically. But this had issues when running on an ARM U22 VM. We
    don't need to manually set the params of the vehicle since it should
    already be contained in autoware_launch.twizy

--- autoware/entrypoint.sh
+++ autoware/entrypoint.sh
@@ -21,45 +21,6 @@ if [ "$ENABLE_VNC" = "true" ]; then
   /start_vnc.sh &
 fi
 
-# Set default overrides to Autoware vehicle parameters so that simulations drive like Twizy
-# This doesn't prevent further overrides within the container (e.g. when building & sourcing launch files in the container)
-set_param () {
-  local FILE=$1
-  local KEY=$2
-  local VALUE=$3
-
-  if [ ! -f "$FILE" ]; then
-    echo "ERROR: File not found: $FILE"
-    exit 1
-  fi
-
-  local current
-  current="$(yq e "$KEY" "$FILE")"
-  if [ "$current" = "null" ] || [ -z "$current" ]; then
-    echo "ERROR: Key not found: $KEY in $FILE"
-    exit 1
-  fi
-
-  yq e "$KEY = $VALUE" -i "$FILE"
-  echo "Updated $KEY → $VALUE"
-}
-
-# Derived from: https://github.com/Monash-Connected-Autonomous-Vehicle/autoware_launch.twizy/blob/mcav-0.45.3/autoware_launch/config/planning/scenario_planning/common/common.param.yaml
-common_param_file="/opt/autoware/share/autoware_launch/config/planning/scenario_planning/common/common.param.yaml"
-set_param $common_param_file '.["/**"].ros__parameters.max_vel' '11.1'
-set_param $common_param_file '.["/**"].ros__parameters.normal.min_acc' '-1.5'
-set_param $common_param_file '.["/**"].ros__parameters.normal.max_acc' '1.5'
-
-# Derived from: https://github.com/Monash-Connected-Autonomous-Vehicle/autoware_launch.twizy/blob/mcav-0.45.3/vehicle/twizy_vehicle_launch/twizy_vehicle_description/config/vehicle_info.param.yaml
-vehicle_info_file="/opt/autoware/share/sample_vehicle_description/config/vehicle_info.param.yaml"
-set_param $vehicle_info_file '.["/**"].ros__parameters.wheel_radius' '0.2725' 
-set_param $vehicle_info_file '.["/**"].ros__parameters.wheel_width' '0.146'
-set_param $vehicle_info_file '.["/**"].ros__parameters.wheel_base' '1.68'
-set_param $vehicle_info_file '.["/**"].ros__parameters.wheel_tread' '1.095'
-set_param $vehicle_info_file '.["/**"].ros__parameters.front_overhang' '0.31'
-set_param $vehicle_info_file '.["/**"].ros__parameters.rear_overhang' '0.35'
-set_param $vehicle_info_file '.["/**"].ros__parameters.vehicle_height' '1.686'
-
 if [ $# -gt 0 ]; then
   exec "$@"
 fi
